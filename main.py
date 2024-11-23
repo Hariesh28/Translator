87ee83a9723d8ebbd974dfbba40f631390cfed91
Hariesh <hariesh23110344@snuchennai.edu.in>
2024-11-23 23:04:07 +0530

    Added Cleanup
    Deletes all the temporary audio files created with the program exits

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import streamlit as st
 from googletrans import Translator, LANGCODES, LANGUAGES
 from gtts import gTTS
 import tempfile
+import atexit
+import os
+
+tempFiles = []
+
+def cleanup():
+
+    for tempFile in tempFiles:
+        try:
+            os.remove(tempFile)
+
+        except OSError as e:
+            print(f"Error Removing File {tempFile}: {e}")
+
+    tempFiles.clear()
 
 def clean_input(text:str) -> str | None:
 
     if not text or not text.strip() or not isinstance(text, str) or '\x00' in text: return None
 
     return text.strip().lower()
-
 
 def translate_text(text:str, dest:str, src:str='auto-detect') -> tuple[str, str|None] | None:
 
@@ -67,6 +81,7 @@ def text_to_speech(text:str, lang:str) -> str | None:
             delete=False
         ) as temporaryFile:
 
+            tempFiles.append(temporaryFile.name)
             tts.save(temporaryFile.name)
             return temporaryFile.name
 
@@ -110,74 +125,78 @@ def update_audio(text:str, lang:str) -> None:
             autoplay=False
         )
 
-st.set_page_config(
-    page_title='Cultural Connect',
-    page_icon='images\\page_icon.png',
-    layout='wide'
-)
+if __name__ == '__main__':
 
-titleCol, imageCol = st.columns(spec=[1, 7], gap='small', vertical_alignment='top')
-with titleCol:st.title("Translate")
-with imageCol: st.image('images\\title_icon.png', width=64)
+    atexit.register(cleanup)
 
-col1, col2 = st.columns(spec=2, gap='medium', vertical_alignment='top')
+    st.set_page_config(
+        page_title='Cultural Connect',
+        page_icon='images\\page_icon.png',
+        layout='wide'
+    )
 
-with col1:
-    sourceLang = st.selectbox(
-        label='Source',
-        options=['auto-detect'] + list(LANGCODES.keys()),
-        index=0,
-        key='source',
-        help='Select the source of the input text',
-        placeholder='Choose the Source Language',
+    titleCol, imageCol = st.columns(spec=[1, 7], gap='small', vertical_alignment='top')
+    with titleCol:st.title("Translate")
+    with imageCol: st.image('images\\title_icon.png', width=64)
+
+    col1, col2 = st.columns(spec=2, gap='medium', vertical_alignment='top')
+
+    with col1:
+        sourceLang = st.selectbox(
+            label='Source',
+            options=['auto-detect'] + list(LANGCODES.keys()),
+            index=0,
+            key='source',
+            help='Select the source of the input text',
+            placeholder='Choose the Source Language',
+            disabled=False,
+            label_visibility='visible'
+        )
+
+    with col2:
+        destinationLang = st.selectbox(
+            label='Destination',
+            options=LANGUAGES.values(),
+            index=list(LANGUAGES.values()).index('tamil'),
+            key='destination',
+            help='Select the Destination of the input text',
+            placeholder='Choose the Destination Language',
+            disabled=False,
+            label_visibility='visible'
+        )
+
+    inputText = st.text_area(
+        label='Enter Text',
+        height=200,
+        key='inputText',
+        help='Enter the text to translate',
+        placeholder='Type something !',
         disabled=False,
         label_visibility='visible'
     )
 
-with col2:
-    destinationLang = st.selectbox(
-        label='Destination',
-        options=LANGUAGES.values(),
-        index=list(LANGUAGES.values()).index('tamil'),
-        key='destination',
-        help='Select the Destination of the input text',
-        placeholder='Choose the Destination Language',
-        disabled=False,
-        label_visibility='visible'
+    sourceAudio = st.empty()
+
+    placeHolder = st.empty()
+    update_translated_text()
+
+    destinationAudio = st.empty()
+
+    if inputText:
+
+        with sourceAudio: update_audio(text=inputText, lang=sourceLang)
+
+        translatedText, srcCode = translate_text(text=inputText, dest=destinationLang, src=sourceLang)
+        if translatedText: update_translated_text(text=translatedText, langCode=srcCode)
+
+        with destinationAudio: update_audio(text=translatedText, lang=destinationLang)
+
+    st.markdown(
+        """
+        ---
+        <div style="text-align: center;  margin-top: 50px;">
+            Built with ❤️ by <a href="https://github.com/Hariesh28" target="_blank">Hariesh</a>
+        </div>
+        """,
+        unsafe_allow_html=True
     )
-
-inputText = st.text_area(
-    label='Enter Text',
-    height=200,
-    key='inputText',
-    help='Enter the text to translate',
-    placeholder='Type something !',
-    disabled=False,
-    label_visibility='visible'
-)
-
-sourceAudio = st.empty()
-
-placeHolder = st.empty()
-update_translated_text()
-
-destinationAudio = st.empty()
-
-if inputText:
-
-    with sourceAudio: update_audio(text=inputText, lang=sourceLang)
-
-    translatedText, srcCode = translate_text(text=inputText, dest=destinationLang, src=sourceLang)
-    if translatedText: update_translated_text(text=translatedText, langCode=srcCode)
-
-    with destinationAudio: update_audio(text=translatedText, lang=destinationLang)
-
-st.markdown(
-    """
-    ---
-    <div style="text-align: center;  margin-top: 50px;">
-        Built with ❤️ by <a href="https://github.com/Hariesh28" target="_blank">Hariesh</a>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
